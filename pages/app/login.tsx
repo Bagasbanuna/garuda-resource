226cab059a194a2430d0220b79be0c7ef55a8a30
Bagas Banuna <bagasbanuna12@gmail.com>
2022-12-14 14:42:17 +0800

    Login page prosses

---
+++ pages/app/login.tsx
@@ -0,0 +1,13 @@
+import { AppShell } from "@mantine/core";
+
+
+function Login(){
+    return (
+        <AppShell>
+            hehe
+        </AppShell>
+    )
+
+}
+
+export default Login
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { HeroContentLeft } from './app/landing'
 
 export default function Home() {
   return (
@@ -11,12 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-
-        
-
-
-
+      <main >
+        <HeroContentLeft/>
 
         {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
